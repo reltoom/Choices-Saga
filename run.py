de8e04e06b0398a2b2e495f91aa79aa4170dc8a0
reltoom <ariel.toom@gmail.com>
2024-03-26 14:11:18 +0100

    Added comments to intro function

--- run.py
+++ run.py
@@ -1,59 +1,79 @@
+#Welcome message before intro
 print('Welcome to the Choices Saga!\n')
 print('You are about to start your journey but before you do we need to know a few things about you.\n')
 
+#Creates the 'player' dictonary with blank values
 player = {
     'name': '',
     'languages': '',
     'shoes': '',
     'color': ''
 }
+
+#Intro function that asks starting question of the player
 def intro():
+    #In an outer loop until player answers 'Yes'
     while True:
+        #Question for players name input and control of answer
         while True:
             playerName = input('Please tell us your name:\n')
 
+            #Controls if player name is only alphabetic
             if playerName.isalpha():
                 player.update({'name': playerName})
                 print('Welcome ' + player['name'] + '!\n')
                 break   
+            #If input contains other than letters
             else:
                 print('Please enter a name with only letters!\n')
 
+        #Setting up next question
         print('Could you tell us which of these languages you know:')
 
+        #Question about which languages the player knows and control
         while True:
             playerLanguages = input('Which of these languages do you know: English, Swedish, Estonian\n')
+            #Takes away the commas from the answers
             known_languages = [language.strip() for language in playerLanguages.split(',')]
 
+            #Checks each language for exact match, adds it to player dict and joins them together with 'and' if needed
             if all(language in ['English', 'Swedish', 'Estonian'] for language in known_languages):
                 player.update({'languages': ' and '.join(known_languages)})
                 print('So you know: ' + player['languages'] + '.\n')
                 break
+            #If input does not exactly match what is required 
             else:
                 print('Type each language exactly as shown with a comma between if more than one.')
 
+        #Question about favorite color and control of answer
         while True:
             favoriteColor = input('What is your favorite color ' + player['name'] + '?\n')
 
+            #Checks if input is only letters
             if favoriteColor.isalpha():
                 player.update({'color': favoriteColor})
                 print('Wow, ' + player['color'] + ' is a very nice color!\n')
-                break   
+                break 
+            #If input contains other than letters  
             else:
                 print('Please enter a color using only letters!\n')
 
+        #Question for correct data input by player
         while True:
             start = input('Now is everything you entered here correct?\nIf so, we can start your journey: Yes or No?\n')
 
+            #If 'Yes' input, start story
             if start == 'Yes':
                 print('Read first area of text file here')
                 return
 
+            #If 'No' input, then restart intro questions
             elif start == 'No':
                 for key in player:
                     player[key] = ''
                 print('Okay, we will begin again.\n\n')
                 break
+            #If input is anything else, reask.
             else:
                 print('Type "Yes" or "No"')
 
