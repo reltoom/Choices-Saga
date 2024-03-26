f5a10fe09224e1b47bcdd2e510896cffdfbf8c41
reltoom <ariel.toom@gmail.com>
2024-03-26 13:58:38 +0100

    Added while True for favorite color and is everything correct question
    If 'No' will restart intro loop. if 'Yes' will return and start story.

--- run.py
+++ run.py
@@ -7,36 +7,57 @@ player = {
     'shoes': '',
     'color': ''
 }
+def intro():
+    while True:
+        while True:
+            playerName = input('Please tell us your name:\n')
 
-while True:
-    playerName = input('Please tell us your name:\n')
+            if playerName.isalpha():
+                player.update({'name': playerName})
+                print('Welcome ' + player['name'] + '!\n')
+                break   
+            else:
+                print('Please enter a name with only letters!\n')
 
-    if playerName.isalpha():
-        player.update({'name': playerName})
-        print('Welcome ' + player['name'] + '!\n')
-        break   
-    else:
-        print('Please enter a name with only letters!\n')
+        print('Could you tell us which of these languages you know:')
 
-print('Could you tell us which of these languages you know:')
+        while True:
+            playerLanguages = input('Which of these languages do you know: English, Swedish, Estonian\n')
+            known_languages = [language.strip() for language in playerLanguages.split(',')]
 
-while True:
-    playerLanguages = input('Which of these languages do you know: English, Swedish, Estonian\n')
-    known_languages = [language.strip() for language in playerLanguages.split(',')]
+            if all(language in ['English', 'Swedish', 'Estonian'] for language in known_languages):
+                player.update({'languages': ' and '.join(known_languages)})
+                print('So you know: ' + player['languages'] + '.\n')
+                break
+            else:
+                print('Type each language exactly as shown with a comma between if more than one.')
 
-    if all(language in ['English', 'Swedish', 'Estonian'] for language in known_languages):
-        player.update({'languages': ' and '.join(known_languages)})
-        print('So you know: ' + player['languages'] + '.\n')
-        break
-    else:
-        print('Please type the languages exactly as shown with a comma between if more than one.')
+        while True:
+            favoriteColor = input('What is your favorite color ' + player['name'] + '?\n')
 
-favoriteColor = input('What is your favorite color ' + player['name'] + '?\n')
-player.update({'color': favoriteColor})
+            if favoriteColor.isalpha():
+                player.update({'color': favoriteColor})
+                print('Wow, ' + player['color'] + ' is a very nice color!\n')
+                break   
+            else:
+                print('Please enter a color using only letters!\n')
 
-print('Wow, ' + player['color'] + ' is a very nice choice!\n\n')
-start = input('Now is everything you entered here correct? If so, we can start your journey: Yes or No?\n')
+        while True:
+            start = input('Now is everything you entered here correct?\nIf so, we can start your journey: Yes or No?\n')
 
+            if start == 'Yes':
+                print('Read first area of text file here')
+                return
+
+            elif start == 'No':
+                for key in player:
+                    player[key] = ''
+                print('Okay, we will begin again.\n\n')
+                break
+            else:
+                print('Type "Yes" or "No"')
+
+intro()
 
 
 
